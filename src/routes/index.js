106c728c75d5e244bace40fddc54884fc7350529
manhd89 <150607244+manhd89@users.noreply.github.com>
2025-06-11 22:45:41 +0700

    thử tự sửa lỗi

--- src/routes/index.js
+++ src/routes/index.js
@@ -56,7 +56,6 @@ async function processMovies(movies, baseUrl) {
       text: is_series ? 'Series' : 'Movie',
       dataKey: `${baseUrl}/${is_series ? 'movie_series' : 'movie_single'}`,
       data: { ...data, ...extraData }
-    ],
     ...(detail.movieDetails?.category || movie.category || []).map(cat => ({
       '@type': 'default',
       type: 'movie',
