5586ca66a90b9242001430c5bea893c071dffa31
manhd89 <150607244+manhd89@users.noreply.github.com>
2025-06-12 14:15:59 +0700

    Cập nhật thông tin

--- src/config.js
+++ src/config.js
@@ -18,19 +18,4 @@ module.exports = {
     timeout: 10000,
     defaultParams: { sort_field: '_id', sort_type: 'asc' },
   },
-
-  // Time-to-live (TTL) settings for caching (in seconds)
-  TTL: {
-    CATEGORIES: 30 * 24 * 60 * 60, // 30 days
-    COUNTRIES: 30 * 24 * 60 * 60, // 30 days
-    NEW_MOVIES: 6 * 60 * 60, // 6 hours
-    SERIES: 1 * 60 * 60, // 1 hour
-    MOVIE_DETAIL: 30 * 24 * 60 * 60, // 30 days (reduced from 68 years for practicality)
-    ONGOING_SERIES: 1 * 60 * 60, // 1 hour
-    PROCESSED_MOVIES: 1 * 60 * 60, // 1 hour
-    HOMEPAGE: 1 * 60 * 60, // 1 hour
-    SEARCH: 15 * 60, // 15 minutes
-    SUGGEST: 15 * 60, // 15 minutes
-    SLUGS: 24 * 60 * 60, // 24 hours
-  },
 };
